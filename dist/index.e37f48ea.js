857335e88ae9d97eb83923cae081ef7ea9caa57a
I PUTU AGUS SETIAWAN <agussetiawaniputu@gmail.com>
2022-04-16 14:21:57 +0800

    fix bug search result

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -1743,8 +1743,8 @@ const loadSearchResults = async function(query) {
 };
 const getSearchResultsPage = function(page = state.search.page) {
     state.search.page = page;
-    const start = (page - 1) * state.search.resultsPerPage;
-    const end = page * state.search.resultsPerPage;
+    const start = (page - 1) * state.search.resultsPerPage; // 0
+    const end = page * state.search.resultsPerPage; // 9
     return state.search.results.slice(start, end);
 };
 const updateServings = function(newServings) {
--- src/js/model.js
+++ src/js/model.js
@@ -54,12 +54,15 @@ export const loadSearchResults = async function (query) {
     }
 };
 
-export const getSearchResultsPage=function(page= state.search.page){
-    state.search.page=page;
-    const start=(page -1 )* state.search.resultsPerPage;
-    const end=page*state.search.resultsPerPage;
-    return state.search.results.slice(start,end);
-}
+
+export const getSearchResultsPage = function (page = state.search.page) {
+    state.search.page = page;
+  
+    const start = (page - 1) * state.search.resultsPerPage; // 0
+    const end = page * state.search.resultsPerPage; // 9
+  
+    return state.search.results.slice(start, end);
+  };
 
 export const updateServings = function (newServings) {
     state.recipe.ingredients.forEach(ing => {
